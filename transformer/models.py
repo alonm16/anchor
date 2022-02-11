f439e5764b823cbc2cbac099647eccbc5b40eab8
Alon Mor <STAFF\almr16@tdk-bm2.csf.technion.ac.il>
2022-02-11 21:24:30 +0200

    added statistics to embedding

--- transformer/models.py
+++ transformer/models.py
@@ -3,38 +3,40 @@ import torch.nn as nn
 from torchtext.vocab import GloVe
 import math
 
-def create_embedding_layer(input_vocabulary, embedding_dim):
+def create_embedding_layer(input_vocabulary, embedding_dim, statistics):
     embedding_glove = GloVe(name='6B', dim=embedding_dim)
     vocab_size = len(input_vocabulary)
-    weight_matrix = torch.zeros((vocab_size, embedding_dim))
+    weight_matrix = torch.zeros((vocab_size, embedding_dim + len(statistics)))
     embedding_glove_itos = embedding_glove.itos
     indices_to_zero = []
     for i, word in enumerate(input_vocabulary.itos):
         if word in embedding_glove_itos:
             
-            weight_matrix[i] = embedding_glove[word]
+            weight_matrix[i, :-len(statistics)] = embedding_glove[word]
+            weight_matrix[i, -2] = statistics[0][word]
+            weight_matrix[i, -1] = statistics[1][word]
             indices_to_zero.append(i)
         else:
-            weight_matrix[i] = torch.normal(mean=0.0, std=1.0, size=(1, embedding_dim))
+            weight_matrix[i] = torch.normal(mean=0.0, std=1.0, size=(1, embedding_dim+ len(statistics)))
             
     
     #pad token is zeros and unk token is average
-    weight_matrix[1] = torch.zeros(size=(1, embedding_dim))
+    weight_matrix[1] = torch.zeros(size=(1, embedding_dim + len(statistics)))
     weight_matrix[0] = torch.mean(weight_matrix, dim=0)
     
     embedding_layer = nn.Embedding.from_pretrained(weight_matrix)
-    embedding_layer.requires_grad_(True)
+    #embedding_layer.requires_grad_(True)
 
     return embedding_layer, indices_to_zero
 
 
 class VanillaGRU(nn.Module):
-    def __init__(self, input_vocabulary, embedding_dim, hidden_dim, num_layers, output_dim, dropout):
+    def __init__(self, input_vocabulary, embedding_dim, hidden_dim, num_layers, output_dim, dropout, statistics):
         super().__init__()
 
-        self.embedding_layer, self.indices_to_zero = create_embedding_layer(input_vocabulary, embedding_dim)
+        self.embedding_layer, self.indices_to_zero = create_embedding_layer(input_vocabulary, embedding_dim, statistics)
         
-        self.GRU_layer = nn.GRU(input_size=embedding_dim, hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout)
+        self.GRU_layer = nn.GRU(input_size=embedding_dim+len(statistics), hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout)
 
         self.dropout_layer = nn.Dropout(dropout)
 
