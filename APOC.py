842550749239b1b3fd68618c8410be34f4287858
Alon Mor <STAFF\almr16@tdk-bm2.csf.technion.ac.il>
2022-09-23 01:07:49 +0300

    fixed apoc

--- APOC.py
+++ APOC.py
@@ -8,53 +8,48 @@ import copy
 from myUtils import set_seed
 
 class APOC:
-    def __init__(self, model, tokenizer, sentences, labels, pos_tokens, neg_tokens, title):
+    def __init__(self, model, tokenizer, sentences, labels, pos_tokens, neg_tokens, title, num_removes = 10):
         self.model = model
         self.tokenizer = tokenizer
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
         self.softmax = torch.nn.Softmax()
-        self.pos_tokens = pos_tokens
-        self.neg_tokens = neg_tokens
         self.formula_type = 'v1'
         self.tokens_method = self._remove_tokens
         self.title = title
+        self.num_removes = num_removes
         
-        self.pos_sentences, self.pos_shuffled_tokens, self.pos_reversed_tokens, self.pos_w = APOC.prepare_apoc(tokenizer, pos_tokens, sentences, labels, 1)
-        self.neg_sentences, self.neg_shuffled_tokens, self.neg_reversed_tokens, self.neg_w = APOC.prepare_apoc(tokenizer, neg_tokens, sentences, labels, 0)
-        
-        self.cur_w = None
-        
-    @staticmethod
-    def _calculate_w(sentences, tokens):
-        tokens_counter = Counter()
-        for s in sentences:
-            tokens_counter.update(s)
-        tokens_counter = {k: v for k, v in tokens_counter.items() if k in tokens}
-        max_occur = max(tokens_counter.values())
-        w = np.ones(len(sentences))
-        for i, s in enumerate(sentences):
-            for t in tokens:
-                if t in s:
-                    w[i] = max(w[i], max_occur/tokens_counter[t])
-        return w
+        self.pos_sentences, self.pos_tokens, self.pos_shuffled_tokens, self.pos_reversed_tokens = APOC.prepare_apoc(tokenizer, pos_tokens, sentences, labels, 1)
+        self.neg_sentences, self.neg_tokens, self.neg_shuffled_tokens, self.neg_reversed_tokens = APOC.prepare_apoc(tokenizer, neg_tokens, sentences, labels, 0)
     
     @staticmethod
     def prepare_apoc(tokenizer, tokens, sentences, labels, desired_label):
+        def get_tokens_arr(s_arr, t_arr, reverse = False):
+            sentences_tokens = []
+            for sentence in s_arr:
+                sentence_tokens = [t for t in t_arr if t in sentence]
+                sentence_tokens.sort(key = lambda x: t_arr.index(x))
+                # for not anchor words in sentence doesn't mean the order of removals because their score is 0
+                not_anchors = [w for w in sentence if w not in sentence_tokens]
+                if not reverse:
+                    sentence_tokens.extend(not_anchors)
+                else:
+                    not_anchors.extend(sentence_tokens)
+                    sentence_tokens = not_anchors
+                sentences_tokens.append(sentence_tokens)
+            return sentences_tokens
+        
         sentences = [tokenizer.tokenize(s) for i, s in enumerate(sentences) if labels[i]==desired_label]
-        # need only sentences with anchors
-        sentences = [s for s in sentences if any(t in s for t in tokens)]
-                    
+        sentences_tokens = get_tokens_arr(sentences, tokens)
+
         shuffled_tokens_arr = []
         for i in range(1,6):
-            set_seed(i)
+            set_seed(i*100)
             shuffled_tokens = tokens.copy()
             random.shuffle(shuffled_tokens)
-            shuffled_tokens_arr.append(shuffled_tokens)
-        reversed_tokens = tokens[::-1]
-        w = APOC._calculate_w(sentences, tokens)
-        
-        return sentences, shuffled_tokens_arr, reversed_tokens, w
+            shuffled_tokens_arr.append(get_tokens_arr(sentences, shuffled_tokens))
+        reversed_tokens = get_tokens_arr(sentences, tokens[::-1], reverse=True)      
+        return sentences, sentences_tokens, shuffled_tokens_arr, reversed_tokens
     
     def _predict_scores(self, sentences):
         encoded = [[101] +[self.tokenizer.vocab[token] for token in tokens] + [102]         
@@ -63,16 +58,16 @@ class APOC:
         outputs = self.softmax(self.model(to_pred)[0])
         return outputs.detach().cpu().numpy()
 
-    def _remove_tokens(self, removed_tokens, sentences):
-        return [['[PAD]' if token in removed_tokens else token for token in sentence] for sentence in sentences]
+    def _remove_tokens(self, idx, tokens_arr, sentences):
+        return [['[PAD]' if token in tokens[:idx] else token for token in sentence] for tokens, sentence in zip(tokens_arr, sentences)]
     
-    def _replace_tokens(self, removed_tokens, sentences):
+    def _replace_tokens(self, idx, tokens_arr, sentences):
         replaced_sentences = []
-        for s in sentences:
+        for tokens, s in zip(tokens_arr, sentences):
             tokenized_sentence = copy.deepcopy(s)
             for i in range(len(tokenized_sentence)):
                 token = tokenized_sentence[i]
-                if token in removed_tokens:
+                if token in tokens[:idx]:
                     tokenized_sentence[i] = '[MASK]'
                     sentence = self.tokenizer.decode(self.tokenizer.encode(tokenized_sentence))
                     results = self.unmasker(sentence, top_k=2)
@@ -93,7 +88,7 @@ class APOC:
     
     def _apoc_formula(self, orig_predictions, predictions_arr, k):
         N = len(orig_predictions)    
-        return sum([sum((orig_predictions - predictions_arr[i]))/N for i in range(k+1)])/(k+1)
+        return sum([sum(orig_predictions - predictions_arr[i])/N for i in range(k+1)])/(k+1)
     
     def _apoc_formula_v2(self, orig_predictions, predictions_arr, k):
         N = len(orig_predictions)
@@ -106,11 +101,11 @@ class APOC:
         
         return [formula_fn(orig_predictions, predictions_arr, k) for k in range(len(predictions_arr))]
 
-    def _apoc_global(self, tokens_to_remove, sentences, labels):
+    def _apoc_global(self, tokens, sentences, labels):
         removed_sentences_arr = []
         removed_sentences = sentences
-        for i in range(len(tokens_to_remove)+1):
-            removed_sentences = self.tokens_method(tokens_to_remove[:i], removed_sentences)
+        for i in range(self.num_removes+1):
+            removed_sentences = self.tokens_method(i, tokens, removed_sentences)
             removed_sentences_arr.append(removed_sentences) 
         predictions_arr = self._apoc_predictions(removed_sentences_arr, labels)
         apoc_scores = self._calc_apoc(predictions_arr)
@@ -132,9 +127,8 @@ class APOC:
         self.tokens_method = self._remove_tokens if tokens_method=='remove' else self._replace_tokens
         legends =  ['regular', 'random', 'reverse']
         
-        self.cur_w = self.pos_w
         normal_scores = self._apoc_global(self.pos_tokens, self.pos_sentences, [1]*len(self.pos_sentences))
-        random_scores = np.zeros(len(self.pos_tokens)+1)
+        random_scores = np.zeros(self.num_removes+1)
         for i in range(5):
             random_scores += np.array(self._apoc_global(self.pos_shuffled_tokens[i], self.pos_sentences, [1]*len(self.pos_sentences)))
         random_scores/=5
@@ -142,9 +136,8 @@ class APOC:
         
         self._plot_apoc([normal_scores, random_scores, reverse_scores], legends , f' positive - {formula_type}' )
         
-        self.cur_w = self.neg_w
         normal_scores = self._apoc_global(self.neg_tokens, self.neg_sentences, [0]*len(self.neg_sentences))
-        random_scores = np.zeros(len(self.neg_tokens)+1)
+        random_scores = np.zeros(self.num_removes+1)
         for i in range(5):
             random_scores += np.array(self._apoc_global(self.neg_shuffled_tokens[i], self.neg_sentences, [0]*len(self.neg_sentences)))
         random_scores/=5
