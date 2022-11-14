61ff12d6685f639b60f8700a463046e65d15c93b
Alon Mor <STAFF\almr16@tdk-bm2.csf.technion.ac.il>
2022-11-15 01:18:36 +0200

    organized apoc

--- APOC.py
+++ APOC.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from collections import Counter, defaultdict
+from functools import reduce
 from transformers import pipeline
 import copy
+import pandas as pd
 from myUtils import set_seed
 
 class APOC:
     def __init__(self, model, tokenizer, sentences, labels, pos_tokens, neg_tokens, title, num_removes = 25, modified = False):
+        """ modified: using our modified apoc """
         self.model = model
         self.tokenizer = tokenizer
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -31,7 +34,7 @@ class APOC:
             for sentence in s_arr:
                 sentence_tokens = [t for t in t_arr if t in sentence]
                 sentence_tokens.sort(key = lambda x: t_arr.index(x))
-                # for not anchor words in sentence doesn't mean the order of removals because their score is 0
+                # for non anchor words score is 0 so removal order is meaningless
                 not_anchors = [w for w in sentence if w not in sentence_tokens]
                 if not reverse:
                     sentence_tokens.extend(not_anchors)
@@ -148,15 +151,151 @@ class APOC:
         self._plot_apoc([normal_scores, random_scores, reverse_scores], legends, f' negative - {formula_type}')
         
         
-    @classmethod
-    def compare_apocs(cls, model, tokenizer, sentences, labels, pos_tokens_arr, neg_tokens_arr, legends, modified = False): 
+    @staticmethod
+    def compare_apocs(model, tokenizer, compare_list, get_scores_fn, sentences, labels, legends, num_removes = 25, modified = False): 
+        
+        pos_tokens_arr = []
+        neg_tokens_arr = []
+        for item in compare_list:
+            pos_scores, neg_scores = get_scores_fn(item)
+            pos_tokens, neg_tokens = list(pos_scores.keys()), list(neg_scores.keys())
+            pos_tokens_arr.append(pos_tokens)
+            neg_tokens_arr.append(neg_tokens)
+        
         pos_scores = []
         neg_scores = []
         for i in range(len(legends)):
-            apoc = cls(model, tokenizer, sentences, labels, pos_tokens_arr[i], neg_tokens_arr[i], "", modified = modified) 
+            apoc = APOC(model, tokenizer, sentences, labels, pos_tokens_arr[i], neg_tokens_arr[i], "", num_removes = num_removes, modified = modified) 
             pos_scores.append(apoc._apoc_global(apoc.pos_tokens, apoc.pos_sentences, [1]*len(apoc.pos_sentences)))
             neg_scores.append(apoc._apoc_global(apoc.neg_tokens, apoc.neg_sentences, [0]*len(apoc.neg_sentences)))
         
         # doesn't matter which apoc plots it
         apoc._plot_apoc(pos_scores, legends, 'positive')
         apoc._plot_apoc(neg_scores, legends, 'negative')
+        
+
+class ApocUtils:
+    @staticmethod
+    def get_scores_dict(folder_name, top=25, trail_path = "0.1/scores.xlsx", alpha = 0.95):
+        """
+        returns dict of (anchor, score) pairs, and sum of the topk positive/negative
+        """
+        df = pd.read_excel(f'{folder_name}/{trail_path}').drop(0)
+
+        index_prefix = f"{alpha}-" if alpha is not None else ""
+
+        neg_keys = df[f'{index_prefix}negative'].dropna().tolist()
+        neg_values = df.iloc[:, list(df.columns).index(f'{index_prefix}negative')+1].tolist()
+        neg_scores =dict(zip(neg_keys, neg_values))    
+
+        pos_keys = df[f'{index_prefix}positive'].dropna().tolist()
+        pos_values = df.iloc[:, list(df.columns).index(f'{index_prefix}positive')+1].tolist()
+        pos_scores = dict(zip(pos_keys, pos_values))
+
+        return pos_scores, neg_scores
+
+    # get all anchor above 0.95, multiple in a sentence
+    @staticmethod
+    def get_best(explanations):
+        best_exps = dict()
+        for exp in explanations:
+            if exp.precision < 0.95:
+                continue
+            if exp.index not in best_exps.keys():
+                best_exps[exp.index]=[exp]
+            else:
+                best_exps[exp.index].append(exp)
+        print(len(best_exps))
+        return reduce(lambda x,y: x+y, best_exps.values())
+    
+    @staticmethod
+    def get_anchor_occurences(explanations):
+        c = Counter()
+        for exp in explanations:
+            c.update([exp.names[0]])
+
+        return c
+    
+    @staticmethod
+    def get_normal_occurences(sentences, anchor_occurences, tokenizer):
+        c = Counter()
+        for sentence in sentences:
+            c.update(tokenizer.tokenize(sentence))
+
+        #removing occurences of the words as anchor
+        for word in anchor_occurences.keys():
+            c[word]-=anchor_occurences[word]
+
+        return c
+
+    @staticmethod
+    def calculate_sum(anchor_occurences, normal_occurences):
+        sums = dict()
+        sum_occurences = sum(anchor_occurences.values())
+        for word, count in anchor_occurences.items():
+            sums[word] = count/sum_occurences
+
+        return sums
+
+    @staticmethod
+    def calculate_avg(anchor_occurences, normal_occurences):
+        avgs = dict()
+        for word, count in anchor_occurences.items():
+            avgs[word] = count/(anchor_occurences[word]+normal_occurences[word])
+
+        return avgs
+    
+    @staticmethod
+    def calculate_score(folder_name, tokenizer, anchor_examples, explanations, labels, agg_name):
+        aggs = {'sum': ApocUtils.calculate_sum, 'avg': ApocUtils.calculate_avg}
+        columns = ['name', 'anchor score', 'type occurences', 'total occurences','+%', '-%', 'both', 'normal']
+
+        exps = ApocUtils.get_best(explanations)
+        pos_exps = [exp for exp in exps if labels[exp.index]==0]
+        neg_exps = [exp for exp in exps if labels[exp.index]==1]
+
+        anchor_occurences = ApocUtils.get_anchor_occurences(exps)
+        pos_occurences = ApocUtils.get_anchor_occurences(pos_exps)
+        neg_occurences = ApocUtils.get_anchor_occurences(neg_exps)
+
+        normal_occurences = ApocUtils.get_normal_occurences(anchor_examples, anchor_occurences, tokenizer)
+        df_pos, df_neg = [], []
+
+        teta_pos = aggs[agg_name](pos_occurences, normal_occurences)
+        teta_neg = aggs[agg_name](neg_occurences, normal_occurences)
+
+        for anchor, score in teta_pos.items():
+            pos_percent = round((pos_occurences[anchor])/anchor_occurences[anchor], 2)
+            neg_percent = 1-pos_percent
+            both = pos_occurences[anchor]>0 and neg_occurences[anchor]>0
+            df_pos.append([anchor, score , pos_occurences[anchor], anchor_occurences[anchor], pos_percent, neg_percent, both,  normal_occurences[anchor]]) 
+
+
+        for anchor, score in teta_neg.items():
+            pos_percent = round((pos_occurences[anchor])/anchor_occurences[anchor], 2)
+            neg_percent = 1-pos_percent
+            both = pos_occurences[anchor]>0 and neg_occurences[anchor]>0
+            df_neg.append([anchor, score, neg_occurences[anchor], anchor_occurences[anchor], pos_percent, neg_percent, both,  normal_occurences[anchor]])
+
+        df_pos.sort(key=lambda exp: -exp[1])
+        df_neg.sort(key=lambda exp: -exp[1])
+        df_pos = pd.DataFrame(data = df_pos, columns = columns ).set_index('name')
+        df_neg = pd.DataFrame(data = df_neg, columns = columns ).set_index('name')
+
+        writer = pd.ExcelWriter(f'{folder_name}/{agg_name}_scores.xlsx',engine='xlsxwriter') 
+
+        workbook=writer.book
+        worksheet=workbook.add_worksheet('Sheet1')
+        writer.sheets['Sheet1'] = worksheet
+
+        cur_col = 0
+        is_positive = False
+
+        for df in [df_pos, df_neg]:
+            cur_type = 'positive' if is_positive else 'negative'
+            is_positive = not is_positive
+            worksheet.write(0, cur_col, f'{cur_type}')
+            df.to_excel(writer, sheet_name=f'Sheet1', startrow=1, startcol=cur_col)
+            cur_col+= len(columns) + 1
+
+        writer.save()
