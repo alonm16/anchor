b639f58ffdfc7adf9680782b3337bbbf1d51e654
alon <alonm774@gmail.com>
2023-04-16 20:46:16 +0300

    multiple toy-spam variations

--- run.sh
+++ run.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 deltas=(0.1 0.15 0.2 0.35 0.5)
 seeds=(42 84 126 168 210)
-ds="toy-spam"
+ds="home-spam"
 
+for ds in "corona" "dilemma" "toy-spam" "home-spam"; do
+	for delta_val in ${deltas[@]}; do
+		for seed_val in ${seeds[@]}; do
+			python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words masking
+		done
+	done
+done
+
+
+<<c
 for delta_val in ${deltas[@]}; do
 	for seed_val in ${seeds[@]}; do
 		python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val
-	done
+ 	done
 done
 
 for opt in "stop-words" "topk" "desired" "masking"; do
 	for delta_val in ${deltas[@]}; do
 		for seed_val in ${seeds[@]}; do
-			python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization $opt
-		done
-	done
+ 			python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization $opt
+ 		done
+ 	done
 done
 
 for delta_val in ${deltas[@]}; do
@@ -28,7 +38,7 @@ for delta_val in ${deltas[@]}; do
         python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words topk masking
     done
 done
-
+c
 # for s in 10; do
 # 	for d in results1/tinybert/corona/confidence/seed/$s/* ; do
 # 		mkdir --parents results1/tinybert/corona/confidence/$(basename $d)/seed-$s;
