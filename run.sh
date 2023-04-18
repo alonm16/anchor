7256b0cfb22444c3364ba6b78f2f81aa75f41e6e
alon <alonm774@gmail.com>
2023-04-18 13:35:52 +0300

    added stop-words masking runnning

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 deltas=(0.1 0.15 0.2 0.35 0.5)
 seeds=(42 84 126 168 210)
-ds="home-spam"
+ds="toy-spam"
+m_type="logistic"
 
-for ds in "corona" "dilemma" "toy-spam" "home-spam"; do
-	for delta_val in ${deltas[@]}; do
-		for seed_val in ${seeds[@]}; do
-			python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words masking
-		done
-	done
-done
-
-
-<<c
 for delta_val in ${deltas[@]}; do
 	for seed_val in ${seeds[@]}; do
-		python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val
+		python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val
+        python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words masking
+        python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words topk
+        python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words desired
+        python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words topk masking
  	done
 done
 
@@ -27,18 +22,6 @@ for opt in "stop-words" "topk" "desired" "masking"; do
  	done
 done
 
-for delta_val in ${deltas[@]}; do
-	for seed_val in ${seeds[@]}; do
-		python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words topk
-	done
-done
-
-for delta_val in ${deltas[@]}; do
-    for seed_val in ${seeds[@]}; do
-        python run_mp.py --model_type tinybert --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val --optimization stop-words topk masking
-    done
-done
-c
 # for s in 10; do
 # 	for d in results1/tinybert/corona/confidence/seed/$s/* ; do
 # 		mkdir --parents results1/tinybert/corona/confidence/$(basename $d)/seed-$s;
