6b617f9785b699524315d27bd2e5491030bf97c5
alon <alonm774@gmail.com>
2023-06-01 11:19:14 +0300

    half deberta toy-spam

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ deltas=(0.1 0.15 0.2 0.35 0.5)
 seeds=(42 84 126 168 210)
 m_type="deberta"
 
-for ds in "corona" "home-spam" "dilemma"; do
-    for seed_val in ${seeds[@]}; do
+for ds in "toy-spam"; do
+    #for seed_val in ${seeds[@]}; do
+	for seed_val in 210; do
         for delta_val in ${deltas[@]}; do
             python run_mp.py --model_type $m_type --dataset_name $ds --sorting confidence --seed $seed_val --delta $delta_val
             for opt in "stop-words" "topk" "desired" "masking"; do
