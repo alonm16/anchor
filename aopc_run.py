87800a4e729ef68e4db0c9ad16fb55d9617c43f4
Alon Mor <STAFF\almr16@tdk-bm2.csf.technion.ac.il>
2023-06-06 15:11:59 +0300

    plotted toy-spam deberta

--- aopc_run.py
+++ aopc_run.py
@@ -13,21 +13,21 @@ sys.path.append('models')
 warnings.simplefilter("ignore")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #torch._C._jit_set_texpr_fuser_enabled(False)
-model_name = 'huawei-noah/TinyBERT_General_4L_312D'
-#model_name = 'microsoft/deberta-v3-small'
+#model_name = 'huawei-noah/TinyBERT_General_4L_312D'
+model_name = 'microsoft/deberta-v3-small'
 
 sorting = "confidence"
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = False)
 myUtils.tokenizer = tokenizer
 
-#model_type = 'deberta'
-model_type = 'logistic'
+model_type = 'deberta'
+#model_type = 'logistic'
 for ds_name in ['toy-spam']:
     path = f'results/mp/{model_type}/{ds_name}/{sorting}/'
 
-    aopc = AOPC(path, tokenizer)
-    aopc.compare_all( only = ['percents time', 'aopc time'])
+    #aopc = AOPC(path, tokenizer)
+    #aopc.compare_all()
 
     aopc = AOPC(path, tokenizer, base_opt = 'stop-words')
-    aopc.compare_all( only = ['percents time', 'aopc time'])
+    aopc.compare_all(only = ['aopc time', 'percents time'])
 
