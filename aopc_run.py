e5c07d432d109be24d1671965018ccf6c2a37c96
Alon Mor <STAFF\almr16@tdk-bm2.csf.technion.ac.il>
2023-08-07 14:29:24 +0300

    logistic corona scores

--- aopc_run.py
+++ aopc_run.py
@@ -13,21 +13,21 @@ sys.path.append('models')
 warnings.simplefilter("ignore")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #torch._C._jit_set_texpr_fuser_enabled(False)
-#model_name = 'huawei-noah/TinyBERT_General_4L_312D'
-model_name = 'microsoft/deberta-v3-small'
+model_name = 'huawei-noah/TinyBERT_General_4L_312D'
+#model_name = 'microsoft/deberta-v3-small'
 
 sorting = "confidence"
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = False)
 myUtils.tokenizer = tokenizer
 
-model_type = 'deberta'
-#model_type = 'tinybert'
+#model_type = 'deberta'
+model_type = 'logistic'
 
-for ds_name in ['corona', 'toy-spam', 'dilemma', 'home-spam']:
+for ds_name in ['corona']:#, 'toy-spam', 'dilemma', 'home-spam']:
     path = f'results/mp/{model_type}/{ds_name}/{sorting}/'
     
-    aopc = AOPC(path, tokenizer)
-    aopc.compare_all(only = ['aggregation', 'aggregation-aopc time'])
-
+    #aopc = AOPC(path, tokenizer)
+    #aopc.compare_all(only = ['aggregation', 'aggregation-aopc time'])
+    print(ds_name)
     aopc = AOPC(path, tokenizer, base_opt = 'stop-words')
-    aopc.compare_all(only = ['aggregation', 'aggregation-aopc time'])
+    aopc.compare_all(only = ['aopc time', 'optimization', 'percents time'])
